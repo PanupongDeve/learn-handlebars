27bf72bc0136d5881c6f98766adcea8b66ee4dc2
panupongdeve <panupong.dev.web@gmail.com>
2025-01-14 22:16:19 +0000

    add process read write all file in directory

--- libs/read-templates.js
+++ libs/read-templates.js
@@ -1,13 +1,52 @@
 const fs = require('fs');
+const path = require('path');
 const fs_promises = fs.promises
 
-const readTemplate = (path) => {
-    try {
-        const data = fs.readFileSync(path, 'utf8');
-        console.log('data', data);
-    } catch (err) {
-        console.error('Error:', err);
-    }
+
+async function readTemplate(filePath) {
+  try {
+    const data = await fs_promises.readFile(filePath, 'utf8');
+    return data;
+  } catch (err) {
+      console.error('Error Reading Template:', err);
+  }
+}
+
+async function writeOutput(filePath, data) {
+  try {
+    // Create the directory if it doesn't exist
+    fs_promises.mkdir(path.dirname(filePath), { recursive: true });
+    await fs_promises.writeFile(filePath, data, 'utf8');
+  } catch (err) {
+      console.error('Error Write Output:', err);
+  }
+}
+
+
+async function processWriteFilesInDirectory(srcDirectory, destDirectory) {
+  try {
+    const entries = await fs_promises.readdir(srcDirectory, { withFileTypes: true });
+    entries.forEach(async (entry) => {
+      const fullPathRead = `${srcDirectory}/${entry.name}`;
+      const fullPathWrite = `${destDirectory}/${entry.name}`;
+      if (entry.isFile()) {
+        console.log('File:', fullPathRead);
+        await processWriteFile(srcDirectory, destDirectory, entry.name);
+      } else if (entry.isDirectory()) {
+        console.log('DirectoryRead:', fullPathRead);
+        console.log('DirectoryWrite:', fullPathWrite);
+        await processWriteFilesInDirectory(fullPathRead, fullPathWrite);
+      }
+    });
+  } catch (error) {
+    console.error('Error reading directory:', error);
+  }
+}
+
+async function processWriteFile(srcDirectory, destDirectory, filename) {
+  const data = await readTemplate(`${srcDirectory}/${filename}`);
+  await writeOutput(`${destDirectory}/${filename}`, data);
+  
 }
 
 async function listFilesInDirectory(directory) {
@@ -38,4 +77,4 @@ async function listFilesInDirectory(directory) {
 
 
 
-module.exports = { readTemplate, listFilesInDirectory };
+module.exports = { readTemplate, listFilesInDirectory, processWriteFilesInDirectory };
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Handlebars = require("handlebars");
 
 const { readJson } = require("./libs/read-json");
-const { readTemplate, listFilesInDirectory } = require('./libs/read-templates.js');
+const { readTemplate, listFilesInDirectory, processWriteFilesInDirectory } = require('./libs/read-templates.js');
 
 // const template = Handlebars.compile("Name: {{name}} - Age: {{age}}");
 // console.log(template({ name: "Nils", age: 30 }));
@@ -12,8 +12,8 @@ const { readTemplate, listFilesInDirectory } = require('./libs/read-templates.js
 
 
 (async () => {
-    readJson("./variables/node-app.json")
-    readTemplate("./templates/node-app.yml.template")
-    const results =  await listFilesInDirectory("./templates")
-    console.log("All results:",JSON.stringify(results, null, 2) );
+    // readJson("./variables/node-app.json")
+    // readTemplate("./templates/node-app.yml")
+    const results =  await processWriteFilesInDirectory("./templates", "./outputs")
+    
 })();
